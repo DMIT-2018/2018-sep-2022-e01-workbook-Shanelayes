eef32db8b0bbc89fac16935145885342842e4c98
Shane Layes <slayes1@nait.ca>
2022-11-02 20:41:10 -0600

    did some stuff added som enotes in program.cs and updated the connection strings

--- ChinookMusic/WebApp/Program.cs
+++ ChinookMusic/WebApp/Program.cs
@@ -5,10 +5,32 @@ using WebApp.Data;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+//Supplied database connection due to the fact that we created this web app to use individual accounts 
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+
+//Add another GetConnection String to referance our DataBase
+//connection string 
+var connectionStringChinook = builder.Configuration.GetConnectionString("ChinookDB");
+
+//Given for the db Connection to Defaultconnection string 
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(connectionString));
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+
+//Code the dbConnection to the application db Context for chinook
+//The implementation of the connect and registration of the chinook system services 
+//Will be done in the ChinookSystem class library, to accomplish this task we will be using an extension method
+//the extwnsion method will service the IserviceCollection 
+//The extension requires a parameter options.useSqlServerxxx
+//where xx is the connection sstring variable
+//
+
+//builder.Services.ChinookSystemBackendDependencies(options =>
+//              options.UseSqlServer(connectionStringChinook));
+
+
+
 
 builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
     .AddEntityFrameworkStores<ApplicationDbContext>();
